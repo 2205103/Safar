408744a642f56b4ab552a428784dd622d0bd98a2
SARWARxSAMI <2205103@ugrad.cse.buet.ac.bd>
2025-06-01 14:44:26 +0600

    Add files via upload

--- Demo/controller/controller/controller.booking.js
+++ Demo/controller/controller/controller.booking.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 const search=require('../routes/booking/booking.search.js');
+const reserve=require('../routes/booking/booking.reserve.js');
 
 app.use('/search',search);
+app.use('/reserve',reserve);
 
 module.exports=app;
