501e50003bc9c2da1c7caa140feefe3f9e1dde61
SARWARxSAMI <2205103@ugrad.cse.buet.ac.bd>
2025-06-02 16:05:35 +0600

    booking controller added

--- Demo/controller/controller/controller.booking.js
+++ Demo/controller/controller/controller.booking.js
@@ -3,8 +3,10 @@ const app = express();
 
 const search=require('../routes/booking/booking.search.js');
 const reserve=require('../routes/booking/booking.reserve.js');
+const geTicket=require('../routes/booking/booking.geTicket.js');
 
 app.use('/search',search);
 app.use('/reserve',reserve);
+app.use('/getTicket',geTicket);
 
 module.exports=app;
