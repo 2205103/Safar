1ada74a7541ffd87183fc66eee32a50a0fc94f9c
SARWARxSAMI <2205103@ugrad.cse.buet.ac.bd>
2025-06-05 17:49:17 +0600

    Add files via upload

---
+++ Demo/routes/routes/user/user.login.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const client = require('../../pg/database.js');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+
+//jwt token je kew dekhte paarbe, password ba emon kisu rakha jaabe na
+
+
+router.post('/', async (req, res) => {
+    const { username, password } = req.body;
+
+    const result = await client.query(`SELECT user_id, password FROM PASSENGER where username=$1;`, [username]);
+    if (result.rows[0].user_id) {
+        const isValid = await bcrypt.compare(password, result.rows[0].password);
+
+        if (isValid) {
+
+            const token = jwt.sign({
+                    user_id: result.rows[0].user_id
+                },process.env.jwt_secret,{
+                    expiresIn: '1h'
+                });
+                
+            res.status(200).json({
+                "access_token": token
+            });
+        }
+        else {
+            res.status(401).json({
+                "error": "Wrong Password"
+            });
+        }
+    }
+    else {
+        res.status(401).json({
+            "error": "Wrong Username"
+        });
+    }
+});
+
+module.exports = router;
